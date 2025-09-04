8b2ae9c2cfe49fe214871de576637695e893e945
YeetusSnoopy <yeetussnoopy@gmail.com>
2025-09-04 01:23:17 -0400

    fixed lib err

--- lib.lua
+++ lib.lua
@@ -4,10 +4,10 @@ function lib:init()
    
 end
 function lib:postInit(new_file)
-   -- if new_file then
+    if new_file then
         Game:setFlag("purified", 0)
         Game:setFlag("slain", 0)
-   -- end
+   end
 end
 
 return lib
